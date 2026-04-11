dfaa93cc0ab17a38e0f01ac568d8fb70bb3d37ad
Tyler Dunn <ireallylikeshrek@gmail.com>
2026-04-11 04:01:53 -0400

    Add maplestory.io to next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "g.nexonstatic.com",
       },
+      {
+        protocol: "https",
+        hostname: "maplestory.io",
+      },
     ],
   },
 };
