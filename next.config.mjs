f9c0c49200bea6ac7ac4af7b242b0a6056baf877
Tyler Dunn <ireallylikeshrek@gmail.com>
2026-05-16 21:57:31 -0400

    Leverage Next.js features to bring massive Page Speed improvements
    - Use next/font/google instead of google font in CSS
    - Tree-shake chart.js more aggressively
    - Add browserslist for reducing unnecessary polyfills

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,9 @@ const nextConfig = {
     "http://localhost:3000",
     "http://127.0.0.1:3000",
   ],
+  experimental: {
+    optimizePackageImports: ["chart.js", "react-chartjs-2"],
+  },
   images: {
     remotePatterns: [
       {
